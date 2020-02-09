6d3158e6a803968b6c836afff8e3748817e0707d
Jeongwonjae <justv95@naver.com>
2020-02-09 13:19:03 +0900

    add [web_31.py]

---
+++ Webhacking.kr/web_31.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+
+utf="utf-8"
+host="0.0.0.0"
+size=1024
+
+def openServer(host, port, size):
+    server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(0)
+    print("[+] Create server socket : {}".format(port))
+
+    while True:
+        client_socket,addr=server_socket.accept()
+        print("[+] Connect with client. :{}".format(port))
+        data=client_socket.recv(size)
+        decode_data=data.decode(utf)
+        print("[+] [{}] message : {}".format(addr, decode_data))
+
+        #send_data="thisis send data"
+        #send_data_byte=send_data.encode(utf)
+        #client_socket.send(send_data_byte)
+
+    client_socket.close()
+    server_socket.close()
+
+if __name__=='__main__':
+
+    for port in range(10000, 10101):
+        openIt=threading.Thread(target=openServer, args=(host, port, size))
+        openIt.start()
+
+#port check -> netstat -ano
+#port close -> taskkill /f /pid {number}
